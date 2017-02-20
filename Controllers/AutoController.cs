9574986d4fba0ca0ac50a3c73221580378d3d782
Wan Haig <Wan Haig>
2017-02-20 14:09:43 -0500

    Lab04Exercise01

--- Controllers/AutoController.cs
+++ Controllers/AutoController.cs
@@ -1,29 +1,47 @@
  using Microsoft.AspNetCore.Mvc;
+ using FisherInsuranceApi.Data;
+ using FisherInsuranceApi.Models;
  [Route("api/auto/quotes")]    
  public class AutoController : Controller    {   
      // POST api/auto/quotes        
-     [HttpPost]       
-      public IActionResult Post([FromBody]string value)        
+            
+      private IMemoryStore db; 
+      public AutoController(IMemoryStore repo) 
+      {
+        db = repo; 
+        }
+
+      [HttpGet] 
+      public IActionResult GetQuotes() 
+      {
+        return Ok(db.RetrieveAllQuotes); 
+        }
+
+      [HttpPost]
+      public IActionResult Post(int id,[FromBody]Quote quote)        
       {           
-         return Created("", value);        
+         return Ok(db.CreateQuote(quote));        
          }  
 
          // GET api/auto/quotes/5        
-         [HttpGet("{id}")]        
-         public IActionResult Get(int id)        
-         {
-           return Ok("The id is: " + id);        
-           }
+        [HttpGet("{id}")]        
+       public IActionResult Get(int id)        
+        {
+          return Ok(db.RetrieveQuote(id));        
+          }
+
            // PUT api/auto/quotes/id        
-           [HttpPut("{id}")]       
-            public IActionResult Put(int id, [FromBody]string value)        
-            {            
-              return NoContent();        
-              }
-              // DELETE api/auto/quotes/id        
-              [HttpDelete("{id}")]        
-              public IActionResult Delete(int id)        
-              {            
-                return Delete(id);        
-                }
+        [HttpPut("{id}")]       
+         public IActionResult Put([FromBody]Quote quote)        
+         {            
+           return Ok(db.UpdateQuote(quote));        
+           }
+
+             // DELETE api/auto/quotes/id        
+        [HttpDelete("{id}")]        
+        public IActionResult Delete(int id,[FromBody]Quote quote)        
+        {            
+           db.DeleteQuote(id);
+           return Ok();        
+          }
   }
