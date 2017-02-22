cbd2196cba9b813f593bbadde71a17680067b068
Wan Haig <Wan Haig>
2017-02-22 18:46:47 -0500

    Lab05EX1_2

---
+++ wwwroot/app/app.component.ts
@@ -0,0 +1,11 @@
+import {Component} from "@angular/core";
+
+@Component({
+
+    selector: "fisher-insurance",
+
+    template: `<home-page>`
+
+})
+
+export class AppComponent { }
---
+++ wwwroot/app/app.module.ts
@@ -0,0 +1,24 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { HttpModule } from '@angular/http';
+import 'rxjs/Rx';
+
+// import our application component
+import { HomeComponent } from './components/home/home.components';
+import { AppComponent } from './app.component';
+
+@NgModule({
+    declarations: [
+         AppComponent,
+         HomeComponent
+         ],
+    imports: [
+        BrowserModule,
+        HttpModule 
+        ], 
+    providers: [
+        ],
+    bootstrap: [
+         AppComponent
+         ], }) 
+export class AppModule { }
