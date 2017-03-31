5b5a16b963b47a0c7c404d4ca5e1fb757087243a
Wan Haig <Wan Haig>
2017-03-30 23:14:24 -0400

    lab7

--- wwwroot/app/app.module.ts
+++ wwwroot/app/app.module.ts
@@ -1,26 +1,46 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpModule } from '@angular/http';
+import { RouterModule } from '@angular/router';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import 'rxjs/Rx';
-
-// import our application component
-import { HomeComponent } from './components/home/home.components';
 import { AppComponent } from './app.components';
-import{NavBarComponent} from './components/navbar/navbar.components';
+import { HomeComponent } from './components/home/home.components';
+import { NavBarComponent } from './components/navbar/navbar.components';
+import { LoginComponent } from './components/login/login.component';
+import { ClaimsComponent } from './components/claims/claims.component';
+import { QuotesComponent } from './components/quotes/quotes.component';
+
+import { AppRouting } from './app.routing';
+import { AuthService } from "./auth.service";
+import { AuthHttp } from "./auth.http";
+
 
 @NgModule({
     declarations: [
-         AppComponent,
-         HomeComponent,
-         NavBarComponent
-         ],
+        AppComponent,
+        HomeComponent,
+        NavBarComponent,
+        LoginComponent,
+        ClaimsComponent,
+        QuotesComponent
+    ],
     imports: [
         BrowserModule,
-        HttpModule 
-        ], 
+        HttpModule,
+        FormsModule,
+        ReactiveFormsModule,
+        RouterModule,
+        AppRouting
+    ],
     providers: [
-        ],
+        AuthService,
+        AuthHttp,
+        AuthService
+],
+    
     bootstrap: [
-         AppComponent
-         ], }) 
+        AppComponent
+    ],
+})
 export class AppModule { }
